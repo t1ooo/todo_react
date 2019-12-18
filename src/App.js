3137ac6c47566f6058a70c872ca21d9c801b2013
graibn <5966878+t1ooo@users.noreply.github.com>
2019-12-18 15:46:55 +0500

    add react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+// http://todomvc.com/examples/react/#/
+
+import React from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        app
+      </div>
+    );
+  }
+}
