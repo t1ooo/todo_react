6288f471ac16a557341c3eec58de578329ea81c4
graibn <5966878+t1ooo@users.noreply.github.com>
2020-01-01 14:04:29 +0500

    add some test

---
+++ src/App.test.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import {render, unmountComponentAtNode } from 'react-dom';
+/* import ReactTestUtils from 'react-dom/test-utils'; */
+import { act } from 'react-dom/test-utils';
+import { App } from './App';
+import {fireEvent} from '@testing-library/react';
+
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+});
+
+it('App', () => {
+  render(<App />, container);
+});
+
+it('show todo: when there is no tasks', () => {
+  const app = render(<App />, container);
+  expect(container.querySelectorAll('.TodoHeader').length).toBe(1);
+  expect(container.querySelectorAll('.TodoBody').length).toBe(0);
+  expect(container.querySelectorAll('.TodoFooter').length).toBe(0);
+  expect(container.querySelectorAll('.TaskItem').length).toBe(0);
+});
+
+it('add new task', () => {
+  const app = render(<App />, container);
+
+  /* 
+    // work
+    app._addTask("456"); 
+  */
+  
+  var input = document.querySelector('.add-new-task');
+
+  act(() => {
+    /* input.value = "645";
+    input.focus();
+    input.click();
+    input.dispatchEvent(new KeyboardEvent('keydown', {key: "Enter", keyCode: 13, which: 13})); */
+    
+    fireEvent.change(input, { target: { value: '645' } });
+    fireEvent.keyDown(input, {key: "Enter", keyCode: 13, which: 13})
+  });
+
+  expect(container.querySelectorAll('.TaskItem').length).toBe(1);
+});
