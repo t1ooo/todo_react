cf9928ff243a96d5b4572a9db85fa9f59c6936ae
graibn <5966878+t1ooo@users.noreply.github.com>
2020-01-01 14:09:40 +0500

    use replace react-dom with testing-library/react

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,11 @@
 import React from 'react';
-import {render, unmountComponentAtNode } from 'react-dom';
 /* import ReactTestUtils from 'react-dom/test-utils'; */
 import { act } from 'react-dom/test-utils';
 import { App } from './App';
-import {fireEvent} from '@testing-library/react';
-
-let container = null;
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  unmountComponentAtNode(container);
-  container.remove();
-  container = null;
-});
-
-it('App', () => {
-  render(<App />, container);
-});
+import {render, fireEvent} from '@testing-library/react';
 
 it('show todo: when there is no tasks', () => {
-  const app = render(<App />, container);
+  const {container} = render(<App />);
   expect(container.querySelectorAll('.TodoHeader').length).toBe(1);
   expect(container.querySelectorAll('.TodoBody').length).toBe(0);
   expect(container.querySelectorAll('.TodoFooter').length).toBe(0);
@@ -30,14 +13,12 @@ it('show todo: when there is no tasks', () => {
 });
 
 it('add new task', () => {
-  const app = render(<App />, container);
-
+  const {container} = render(<App />);
+  
   var input = container.querySelector('.add-new-task');
 
-  act(() => {
-    fireEvent.change(input, { target: { value: '645' } });
-    fireEvent.keyDown(input, {key: "Enter", keyCode: 13, which: 13})
-  });
+  fireEvent.change(input, { target: { value: '645' } });
+  fireEvent.keyDown(input, {key: "Enter", keyCode: 13, which: 13})
 
   expect(container.querySelectorAll('.TaskItem').length).toBe(1);
 });
