571e45c6aca224a2e0f31866b9293c257621f886
graibn <5966878+t1ooo@users.noreply.github.com>
2020-01-01 15:39:34 +0500

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,63 @@
-import React from 'react';
-/* import ReactTestUtils from 'react-dom/test-utils'; */
-import { act } from 'react-dom/test-utils';
-import { App } from './App';
-import {render, fireEvent} from '@testing-library/react';
+import React from "react";
+import {render as reactRender, unmountComponentAtNode } from 'react-dom';
+/* import { act } from "react-dom/test-utils"; */
+import { App } from "./App";
+import {fireEvent} from "@testing-library/react";
 
-it('show todo: when there is no tasks', () => {
-  const {container} = render(<App />);
-  expect(container.querySelectorAll('.TodoHeader').length).toBe(1);
-  expect(container.querySelectorAll('.TodoBody').length).toBe(0);
-  expect(container.querySelectorAll('.TodoFooter').length).toBe(0);
-  expect(container.querySelectorAll('.TaskItem').length).toBe(0);
+let container = null;
+beforeEach(() => {
+  container = document.createElement('div');
+  document.body.appendChild(container);
 });
 
-it('add new task', () => {
-  const {container} = render(<App />);
+afterEach(() => {
+  unmountComponentAtNode(container);
+  container.remove();
+  container = null;
+  localStorage.removeItem(App._storageKey);
+});
+
+function render(component) {
+  return reactRender(component, container);
+}
+
+it("show todo: when there is no tasks", () => {
+  const app = render(<App />);
+  expect(container.querySelectorAll(".TodoHeader").length).toBe(1);
+  expect(container.querySelectorAll(".TodoBody").length).toBe(0);
+  expect(container.querySelectorAll(".TodoFooter").length).toBe(0);
+});
+
+it("show todo: when there is at least one task", () => {
+  const app = render(<App />);
   
-  var input = container.querySelector('.add-new-task');
-
-  fireEvent.change(input, { target: { value: '645' } });
-  fireEvent.keyDown(input, {key: "Enter", keyCode: 13, which: 13})
-
-  expect(container.querySelectorAll('.TaskItem').length).toBe(1);
+  const text = "new task";
+  addTask(container, text);
+  
+  expect(container.querySelectorAll(".TodoHeader").length).toBe(1);
+  expect(container.querySelectorAll(".TodoBody").length).toBe(1);
+  expect(container.querySelectorAll(".TodoFooter").length).toBe(1);
 });
+
+it("add new task", () => {
+  const app = render(<App />);
+
+  var tasks = container.querySelectorAll(".TaskItem");
+  expect(tasks.length).toBe(0);
+
+  const text = "new task";
+  addTask(container, text);
+
+  var tasks = container.querySelectorAll(".TaskItem");
+  expect(tasks.length).toBe(1);
+
+  const task = tasks.item(0);
+  expect(task.querySelector(".text").textContent).toBe(text);
+  expect(task.querySelector(".toggle").checked).toBe(false); 
+});
+
+function addTask(container, text) {
+  const input = container.querySelector(".add-new-task");
+  fireEvent.change(input, {target: {value: text}});
+  fireEvent.keyDown(input, {key: "Enter", keyCode: 13, which: 13});
+}
