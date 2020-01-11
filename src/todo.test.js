a1b0478a627847f520f40969b3b61e25e9d5912d
graibn <5966878+t1ooo@users.noreply.github.com>
2020-01-11 12:21:14 +0500

    update tests

--- src/todo.test.js
+++ src/todo.test.js
@@ -3,55 +3,18 @@
 import { describe, it, expect, } from 'jest-without-globals';
 import {Todo, Task} from "./todo";
 
-/* describe("test Task", () => {
-  const defaultText = "text";
-  const defaultComleted = true;
-  const defaultId = "id";
-
-  //
-  it.each(["some task"])("no error when text valid", (text) => {
-    new Task(text, defaultComleted, defaultId);
-  });
-
-  it.each(["", null, undefined])("throw error when text invalid", (text) => {
-    expect(() => {
-      new Task(text, defaultComleted, defaultId);
-    }).toThrow("bad text");
-  });
-
-  //
-  it.each([true, false, undefined])("no error when completed valid", (completed) => {
-    new Task(defaultText, completed, defaultId);
-  });
-
-  it.each(["", null])("throw error when completed invalid", (completed) => {
-    expect(() => {
-      new Task(defaultText, completed, defaultId);
-    }).toThrow("bad completed");
-  });
-
-  //
-  it.each(["id", undefined])("no error when id valid", (id) => {
-    new Task(defaultText, defaultComleted, id);
-  });
-
-  it.each(["", null])("throw error when id invalid", (id) => {
-    expect(() => {
-      new Task(defaultText, defaultComleted, id);
-    }).toThrow("bad id");
-  });
-}); */
 describe("new Task", () => {
-  const defaultText = "text";
-  const defaultComleted = true;
-  const defaultId = "id";
-
-  //
-  it.each(["some task"])("no error when text is valid", (text) => {
+  it.each([
+    "some task"
+  ])("no error when text is valid: %p", (text) => {
     new Task(text);
   });
 
-  it.each(["", null, undefined])("throw error when text is invalid", (text) => {
+  it.each([
+    "", 
+    null, 
+    undefined
+  ])("throw error when text is invalid: %p", (text) => {
     expect(() => {
       new Task(text);
     }).toThrow("bad text");
@@ -59,41 +22,68 @@ describe("new Task", () => {
 });
 
 describe("new Task from object", () => {
-  const defaultText = "text";
-  const defaultComleted = true;
-  const defaultId = "id";
-  const newObject = (text, completed, id) => ({text:text, completed:completed, id:id});
+  const defaultObj = {
+    text: "text",
+    completed: true,
+    id: "id"
+  };
+  const updateObj = (key, val) => ({...defaultObj, [key]:val});
 
-  //
-  it.each(["some task"])("no error when text is valid", (text) => {
-    Task.fromObject(newObject(text, defaultComleted, defaultId));
+  // text
+  it.each([
+    "some task"
+  ])("no error when text is valid: %p", (text) => {
+    const obj = updateObj("text", text);
+    Task.fromObject(obj);
   });
 
-  it.each(["", null, undefined])("throw error when text is invalid", (text) => {
+  it.each([
+    "", 
+    null, 
+    undefined
+  ])("throw error when text is invalid: %p", (text) => {
+    const obj = updateObj("text", text);
     expect(() => {
-      Task.fromObject(newObject(text, defaultComleted, defaultId));
+      Task.fromObject(obj);
     }).toThrow("bad text");
   });
 
-  //
-  it.each([true, false])("no error when completed is valid", (completed) => {
-    Task.fromObject(newObject(defaultText, completed, defaultId));
+  // completed
+  it.each([
+    true, 
+    false
+  ])("no error when completed is valid: %p", (completed) => {
+    const obj = updateObj("completed", completed);
+    Task.fromObject(obj);
   });
 
-  it.each(["", null, undefined])("throw error when completed is invalid", (completed) => {
+  it.each([
+    "", 
+    null, 
+    undefined
+  ])("throw error when completed is invalid: %p", (completed) => {
+    const obj = updateObj("completed", completed);
     expect(() => {
-      Task.fromObject(newObject(defaultText, completed, defaultId));
+      Task.fromObject(obj);
     }).toThrow("bad completed");
   });
 
-  //
-  it.each(["id"])("no error when id is valid", (id) => {
-    Task.fromObject(newObject(defaultText, defaultComleted, id));
+  // id
+  it.each([
+    "id"
+  ])("no error when id is valid: %p", (id) => {
+    const obj = updateObj("id", id);
+    Task.fromObject(obj);
   });
 
-  it.each(["", null, undefined])("throw error when id is invalid", (id) => {
+  it.each([
+    "", 
+    null, 
+    undefined
+  ])("throw error when id is invalid: %p", (id) => {
+    const obj = updateObj("id", id);
     expect(() => {
-      Task.fromObject(newObject(defaultText, defaultComleted, id));
+      Task.fromObject(obj);
     }).toThrow("bad id");
   });
 });
